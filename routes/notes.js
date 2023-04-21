3fa524dc0b116ab9be9e1ccfe38d7b979c6f5f5b
Marissa T <marissalynnthompson@gmail.com>
2023-04-21 03:42:37 -0600

    added routes

---
+++ routes/notes.js
@@ -0,0 +1,42 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile, readAndDelete } = require('../helpers/fsUtils');
+const Note = require('../lib/note');
+
+// GET Route for retrieving notes information
+notes.get('/', (req, res) => {
+  // Logic for sending all the content of db/db.json
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+});
+
+// POST Route for a error logging
+notes.post('/', (req, res) => {
+  // Logic for appending data to the db/db.json file
+  if (req.body) {
+    //create Note object
+    
+    let newNote = new Note(uuidv4(), req.body.title, req.body.text);
+
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding the note');
+  }
+});
+
+// POST Route for a error logging
+notes.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+
+    // Logic for appending data to the db/db.json file
+    if (noteId) {
+      readAndDelete(noteId, './db/db.json');
+      res.json(`Note deleted successfully 🚀`);
+    } else {
+      res.error('Error in deleting the note');
+    }
+  }); 
+
+module.exports = notes;
+
